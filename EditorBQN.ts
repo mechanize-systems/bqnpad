4ed4f18ee8572ce4b635427daa7f0333fdaffa43
Andrey Popp <8mayday@gmail.com>
2022-02-25 14:25:32 +0300

    Move BQN keymap to EditorBQN.bqn() extension

--- EditorBQN.ts
+++ EditorBQN.ts
@@ -1,7 +1,11 @@
+/**
+ * Editor support for BQN.
+ */
 import { parser } from "@bqnpad/grammar";
 import { HighlightStyle, Tag, styleTags } from "@codemirror/highlight";
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
-import type { Extension } from "@codemirror/state";
+import * as State from "@codemirror/state";
+import * as View from "@codemirror/view";
 
 let tags = {
   BQNstring: Tag.define(),
@@ -62,12 +66,162 @@ export let language = LRLanguage.define({
   }),
   languageData: {
     commentTokens: { line: "#" },
-    indentOnInput: /^\s*(\]|\}|\)|end|else|elseif|catch|finally)/,
+    indentOnInput: /^\s*(\]|\}|\))/,
     closeBrackets: { brackets: ["(", "[", "{", "'", '"', "`"] },
   },
 });
 
+export let keymap: Map<string, string> = new Map([
+  ["`", "˜"],
+  ["1", "˘"],
+  ["2", "¨"],
+  ["3", "⁼"],
+  ["4", "⌜"],
+  ["5", "´"],
+  ["6", "˝"],
+  ["7", "7"],
+  ["8", "∞"],
+  ["9", "¯"],
+  ["0", "•"],
+  ["-", "÷"],
+  ["=", "×"],
+  ["~", "¬"],
+  ["!", "⎉"],
+  ["@", "⚇"],
+  ["#", "⍟"],
+  ["$", "◶"],
+  ["%", "⊘"],
+  ["^", "⎊"],
+  ["(", "⟨"],
+  [")", "⟩"],
+  ["_", "√"],
+  ["+", "⋆"],
+  ["q", "⌽"],
+  ["w", "𝕨"],
+  ["e", "∊"],
+  ["r", "↑"],
+  ["t", "∧"],
+  ["u", "⊔"],
+  ["i", "⊏"],
+  ["o", "⊐"],
+  ["p", "π"],
+  ["[", "←"],
+  ["]", "→"],
+  ["W", "𝕎"],
+  ["E", "⍷"],
+  ["R", "𝕣"],
+  ["T", "⍋"],
+  ["Y", "Y"],
+  ["U", "U"],
+  ["I", "⊑"],
+  ["O", "⊒"],
+  ["{", "⊣"],
+  ["}", "⊢"],
+  ["a", "⍉"],
+  ["s", "𝕤"],
+  ["d", "↕"],
+  ["f", "𝕗"],
+  ["g", "𝕘"],
+  ["h", "⊸"],
+  ["j", "∘"],
+  ["k", "○"],
+  ["l", "⟜"],
+  [";", "⋄"],
+  ["'", "↩"],
+  ["S", "𝕊"],
+  ["F", "𝔽"],
+  ["G", "𝔾"],
+  ["H", "«"],
+  ["K", "⌾"],
+  ["L", "»"],
+  [",", "·"],
+  ["z", "⥊"],
+  ['"', "˙"],
+  ["x", "𝕩"],
+  ["c", "↓"],
+  ["v", "∨"],
+  ["b", "⌊"],
+  ["m", "≡"],
+  [",", "∾"],
+  [".", "≍"],
+  ["/", "≠"],
+  ["Z", "⋈"],
+  ["X", "𝕏"],
+  ["V", "⍒"],
+  ["B", "⌈"],
+  ["M", "≢"],
+  ["<", "≤"],
+  [">", "≥"],
+  ["?", "⇐"],
+  [" ", "‿"],
+]);
+
+function bqnKeymap(): State.Extension {
+  let expecting: NodeJS.Timeout | null = null;
+
+  let resetExpecting = () => {
+    if (expecting != null) {
+      clearTimeout(expecting);
+      expecting = null;
+    }
+  };
+
+  let scheduleExpecting = () => {
+    resetExpecting();
+    expecting = setTimeout(() => {
+      expecting = null;
+    }, 500);
+  };
+
+  let manageExpecting = View.ViewPlugin.fromClass(
+    class {
+      destroy() {
+        resetExpecting();
+      }
+    },
+  );
+
+  let transactionFilter = State.EditorState.transactionFilter.of((tr) => {
+    if (!tr.isUserEvent("input")) return [tr] as State.TransactionSpec[];
+
+    let pos = null as
+      | { type: "expect" }
+      | { type: "input"; from: number; to: number; ch: string }
+      | null;
+
+    tr.changes.iterChanges((fa, ta, _fb, _tb, ins) => {
+      if (ins.length !== 1) return;
+      let ch = ins.sliceString(0);
+      if (ch === "\\") {
+        pos = { type: "expect" };
+      } else if (
+        tr.startState.doc.sliceString(fa - 1, ta) === "\\" &&
+        expecting != null
+      ) {
+        resetExpecting();
+        pos = { type: "input", from: fa - 1, to: ta, ch };
+      }
+    });
+
+    if (pos?.type === "expect") {
+      scheduleExpecting();
+    } else if (pos?.type === "input") {
+      let insert = keymap.get(pos.ch);
+      if (insert != null)
+        return {
+          changes: { from: pos.from, to: pos.to, insert },
+        } as State.TransactionSpec;
+    }
+    return [tr] as State.TransactionSpec[];
+  });
+
+  return [transactionFilter, manageExpecting];
+}
+
+/**
+ * Configure extension for BQN.
+ */
 export function bqn() {
-  let extensions: Extension[] = [highlight];
+  let extensions: State.Extension[] = [highlight, bqnKeymap()];
   return new LanguageSupport(language, extensions);
 }
