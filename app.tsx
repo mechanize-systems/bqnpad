b180aac3fd8ad6ac70da66e8444d802945e1d062
Andrey Popp <8mayday@gmail.com>
2022-02-26 12:13:20 +0300

    Wording

--- app.tsx
+++ app.tsx
@@ -43,15 +43,15 @@ const INITIAL_DOC = State.Text.of(
 # How to enter BQN glyphs:
 # - Use glyph bar at the top
 # - Use '\\'-prefix to enter glyphs from keyboard.
-# - Enter '\\' and press Tab for completion
+# - Enter '\\' and press Tab for completions
 #
 # How to evaluate BQN expressions:
-# - Expression are being evaluated as you type
+# - Expressions are being evaluated as you type
 # - Press 'Shift+Enter' to create a new cell (the current code is being preserved in
-#   workspace history)
+#   the workspace history)
 #
 # What else:
-# - You can download file with BQN source code to your computer by pressing
+# - You can download workspace code as a .bqn file to your computer by pressing
 #   DOWNLOAD button.
 #
 # Have fun ('Cmd/Ctrl-a' to select all and press 'Del' to remove this message)!
